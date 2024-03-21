e2915b2f940af1e1a6929b9a453ba9af746ef4d7
HelbertAguiar <helbert.aguiar@hotmail.com>
2024-03-20 23:11:57 -0300

    teste3 pipeline

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello Helbert, lets go projeto TEIA 2024!')
+  res.send('Hello Helbert, lets go projeto TEIA 2024! \n A Grazi é foda')
 })
 
 app.listen(port, () => {
