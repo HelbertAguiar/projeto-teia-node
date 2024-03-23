9d32bb515b575e605848ca5e52b0d2a0c1c10ebe
HelbertAguiar <helbert.aguiar@hotmail.com>
2024-03-22 23:34:59 -0300

    :rocket: deploy

--- api/v1/routes/routes.js
+++ api/v1/routes/routes.js
@@ -6,7 +6,7 @@ router.get('/api/manipulacao-string', HandleController.index);
 router.post('/api/manipulacao-string', HandleController.index);
 
 router.get('/', function (req, res) {
-    res.send('Conclusão de MVP do projeto TEIA: "https://projeto-teia-node.azurewebsites.net/api/manipulacao-string?texto=arara"');
+    res.send('Conclusão do desafio do projeto TEIA. Exemplo de requisição GET: "https://projeto-teia-node.azurewebsites.net/api/manipulacao-string?texto=arara"');
 });
 
 export default router;
